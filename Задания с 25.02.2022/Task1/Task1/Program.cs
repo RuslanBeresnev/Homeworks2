1b50ca2c6a81c0583008bcca16cd52dc6f0c2075
RuslanBeresnev <rusber03102003@gmail.com>
2022-02-26 18:03:37 +0300

    Initial Commit

---
+++ Задания с 25.02.2022/Task1/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+internal class Program
+{
+    private static bool GlobalTest()
+    {
+        Trie trie = new Trie();
+        trie.Add("Привет");
+        trie.Add("Проект");
+        trie.Add("Река");
+        trie.Add("Реестр");
+
+        bool test1 = trie.Contains("Привет") && trie.Contains("Проект") && trie.Contains("Река") && trie.Contains("Реестр");
+
+        bool test2 = trie.HowManyStartsWithPrefix("Пр") == 2;
+
+        trie.Remove("Привет");
+        trie.Remove("Реестр");
+
+        bool test3 = !trie.Contains("Привет") && trie.Contains("Проект") && trie.Contains("Река") && !trie.Contains("Реестр");
+
+        bool test4 = trie.Size == 2;
+
+        return test1 && test2 && test3 && test4;
+    }
+
+    public static void Main(string[] args)
+    {
+        if (GlobalTest())
+        {
+            Console.WriteLine("Всё работает");
+        }
+        else
+        {
+            Console.WriteLine("Кажется, что-то не так");
+        }
+    }
+}
---
+++ Задания с 25.02.2022/Task1/Task1/Trie.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Trie
+{
+    private class Node
+    {
+        public Dictionary<char, Node> childs;
+        public bool isTerminal = false;
+
+        internal Node()
+        {
+            childs = new Dictionary<char, Node>();
+        }
+    }
+
+    private Node root = new Node();
+    public int Size = 0;
+
+    public Trie() { }
+
+    public bool Add(string element)
+    {
+        Node current = root;
+        bool containsBefore = true;
+
+        Size += 1;
+
+        foreach (char letter in element)
+        {
+            if (!current.childs.ContainsKey(letter))
+            {
+                current.childs[letter] = new Node();
+                containsBefore = false;
+            }
+            current = current.childs[letter];
+        }
+
+        current.isTerminal = true;
+
+        return !containsBefore;
+    }
+
+    public bool Contains(string element)
+    {
+        Node current = root;
+
+        foreach (char letter in element)
+        {
+            if (!current.childs.ContainsKey(letter))
+            {
+                return false;
+            }
+            current = current.childs[letter];
+        }
+
+        return current.isTerminal;
+    }
+
+    public bool Remove(string element)
+    {
+        Node current = root;
+
+        Node lastNodeWithChilds = root;
+        char firstLetterInLastBranch = '0';
+
+        Size -= 1;
+
+        foreach (char letter in element)
+        {
+            if (!current.childs.ContainsKey(letter))
+            {
+                return false;
+            }
+            if (current.childs.Count > 1)
+            {
+                lastNodeWithChilds = current;
+                firstLetterInLastBranch = letter;
+            }
+            current = current.childs[letter];
+        }
+
+        lastNodeWithChilds.childs.Remove(firstLetterInLastBranch);
+
+        return true;
+    }
+
+    public int HowManyStartsWithPrefix(string prefix)
+    {
+        Node current = root;
+
+        foreach (char letter in prefix)
+        {
+            current = current.childs[letter];
+        }
+
+        return current.childs.Count;
+    }
+}
