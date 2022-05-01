0a01b09bd0c9731d8f296378de41ba5ce08d11b8
RuslanBeresnev <rusber03102003@gmail.com>
2022-05-01 16:20:04 +0300

    Initial Commit

---
+++ Задания с 24.03.2022/Task2/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Game;
+
+/// <summary>
+/// Класс, который запускает игровой процесс
+/// </summary>
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var eventLoop = new EventLoop();
+        var game = new Game("../../../map.txt");
+        game.DrawMap();
+
+        eventLoop.leftHandler += game.OnLeft;
+        eventLoop.rightHandler += game.OnRight;
+        eventLoop.upHandler += game.OnUp;
+        eventLoop.downHandler += game.OnDown;
+
+        eventLoop.Run();
+    }
+}
