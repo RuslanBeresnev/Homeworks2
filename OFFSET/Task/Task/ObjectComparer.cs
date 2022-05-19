867046d8d70421d13a8fce505dfefb46ef2fb7d1
RuslanBeresnev <rusber03102003@gmail.com>
2022-05-19 17:01:30 +0300

    Initial Commit

---
+++ OFFSET/Task/Task/ObjectComparer.cs
@@ -0,0 +1,32 @@
+﻿namespace Offset;
+
+using System.Collections.Generic;
+
+/// <summary>
+/// Класс, реализующий сравнение объектов
+/// </summary>
+public class ObjectComparer<T> : IComparer<T> where T : IComparable<T>
+{
+    /// <summary>
+    /// Сравнение двух объектов одного типа
+    /// </summary>
+    public int Compare(T? x, T? y)
+    {
+        if (x == null)
+        {
+            if (y == null)
+            {
+                return 0;
+            }
+
+            return -1;
+        }
+
+        if (y == null)
+        {
+            return 1;
+        }
+
+        return x.CompareTo(y);
+    }
+}
