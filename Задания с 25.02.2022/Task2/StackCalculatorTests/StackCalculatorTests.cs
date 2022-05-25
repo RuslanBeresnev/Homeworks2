ed7392274f1e575ebee151e17c64f6f7688e5723
RuslanBeresnev <rusber03102003@gmail.com>
2022-05-25 23:49:33 +0300

    Исправлены ошибки в тестах

--- Задания с 25.02.2022/Task2/StackCalculatorTests/StackCalculatorTests.cs
+++ Задания с 25.02.2022/Task2/StackCalculatorTests/StackCalculatorTests.cs
@@ -1,37 +1,37 @@
+namespace StackCalculatorTests;
+
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
 using PostfixCalculator;
-
-namespace StackCalculatorTests;
 
 public class StackCalculatorTests
 {
     private static IEnumerable<TestCaseData> Stacks
         => new TestCaseData[]
         {
-        new TestCaseData(new PostfixCalculator.PostfixCalculator(new ArrayStack())),
-        new TestCaseData(new PostfixCalculator.PostfixCalculator(new ListStack()))
+            new TestCaseData(new PostfixCalculator(new ArrayStack())),
+            new TestCaseData(new PostfixCalculator(new ListStack()))
         };
 
     [TestCaseSource(nameof(Stacks))]
-    public void CalculateCorrectSequence(PostfixCalculator.PostfixCalculator calculator)
+    public void CalculateCorrectSequence(PostfixCalculator calculator)
     {
-        (double? result, bool correctWork) = calculator.CalculateExpression("225 6 7 8 + * /");
-        Assert.IsTrue(correctWork && Math.Abs((double)result - 2.5) < 0.000001);
+        double? result = calculator.CalculateExpression("225 6 7 8 + * /");
+        Assert.True(result != null);
+        Assert.AreEqual(2.5, result!.Value, 0.000001);
     }
 
     [TestCaseSource(nameof(Stacks))]
-    public void CalculateSequenceWithDivisingByZero(PostfixCalculator.PostfixCalculator calculator)
+    public void CalculateSequenceWithDivisingByZero(PostfixCalculator calculator)
     {
-        (double? result, bool correctWork) = calculator.CalculateExpression("5 0 /");
-        Assert.IsFalse(correctWork);
+        double? result = calculator.CalculateExpression("5 0 /");
+        Assert.True(result == null);
     }
 
     [TestCaseSource(nameof(Stacks))]
-    public void CalculteIncorrectSequence(PostfixCalculator.PostfixCalculator calculator)
+    public void CalculteIncorrectSequence(PostfixCalculator calculator)
     {
-        (double? result, bool correctWork) = calculator.CalculateExpression("5 6 7 + - * /");
-        Assert.IsFalse(correctWork);
+        double? result = calculator.CalculateExpression("5 6 7 + - * /");
+        Assert.True(result == null);
     }
 }
--- Задания с 25.02.2022/Task2/StackCalculatorTests/StackTests.cs
+++ Задания с 25.02.2022/Task2/StackCalculatorTests/StackTests.cs
@@ -1,18 +1,19 @@
-﻿using NUnit.Framework;
+﻿namespace StackCalculatorTests;
+
+using NUnit.Framework;
 using System.Collections.Generic;
 using PostfixCalculator;
-
-namespace StackCalculatorTests;
 
 public class StackTests
 {
     private static IEnumerable<TestCaseData> Stacks
         => new TestCaseData[]
         {
-        new TestCaseData(new ArrayStack()),
-        new TestCaseData(new ListStack())
+            new TestCaseData(new ArrayStack()),
+            new TestCaseData(new ListStack())
         };
 
+    [TestCaseSource(nameof(Stacks))]
     public void StackNotEmptyAfterPush(IStack stack)
     {
         stack.Push(1);
@@ -26,18 +27,13 @@ public class StackTests
         stack.Push(2);
         stack.Push(3);
 
-        var values = new double?[3];
-        bool correctWork = true;
-
         for (int i = 0; i < 3; i++)
         {
-            (values[i], bool correctOperation) = stack.Pop();
-            correctWork = correctWork && correctOperation;
+            Assert.AreEqual(3 - i, stack.Pop());
         }
-
-        Assert.True(values[0] == 3 && values[1] == 2 && values[2] == 1);
     }
 
+    [TestCaseSource(nameof(Stacks))]
     public void IsEmptyMethodWorkCorrectly(IStack stack)
     {
         Assert.True(stack.IsEmpty());
@@ -45,9 +41,9 @@ public class StackTests
         Assert.False(stack.IsEmpty());
     }
 
+    [TestCaseSource(nameof(Stacks))]
     public void PopUsingWithEmptyStack(IStack stack)
     {
-        (double? value, bool correctWork) = stack.Pop();
-        Assert.False(correctWork);
+        Assert.AreEqual(null, stack.Pop());
     }
 }
