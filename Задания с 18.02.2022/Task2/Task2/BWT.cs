511dba855af0db7c4a06b7efd10ced1a9a2a7aa7
RuslanBeresnev <rusber03102003@gmail.com>
2022-05-26 19:56:53 +0300

    Модернизированы прямое и обратное преобразование BWT, добавлены комментарии

--- Задания с 18.02.2022/Task2/Task2/BWT.cs
+++ Задания с 18.02.2022/Task2/Task2/BWT.cs
@@ -1,49 +1,113 @@
-﻿using System;
-
-// Прямое и обратное преобразования Барроуза-Уилера
+﻿/// <summary>
+/// Класс, реализующий прямое и обратное преобразования Барроуза-Уилера
+/// </summary>
 public static class BWT
 {
-    private static string LeftShift(string line, int count)
-        => line.Substring(count) + line.Substring(0, count);
-
+    /// <summary>
+    /// Прямое преобразование
+    /// </summary>
     public static (string, int) DirectBWT(string line)
     {
-        var cyclicShifts = new string[line.Length];
-        for (var i = 0; i < line.Length; i++)
+        // Сортированный список с индексами последних символов в каждой циклически сдвинутой строке
+        var sortedLastSymbolsOfStringsIndexes = new List<int>();
+        var currentLine = line;
+        var currentLineLastSymbolIndex = currentLine.Length - 1;
+
+        for (int i = 0; i < line.Length; i++)
         {
-            cyclicShifts[i] = line;
-            line = LeftShift(line, 1);
+            if (i == 0)
+            {
+                sortedLastSymbolsOfStringsIndexes.Add(currentLineLastSymbolIndex);
+            }
+            else
+            {
+                int sortedListIndex = 0;
+                while (true)
+                {
+                    int currentLineInIndexesListStartIndex = (sortedLastSymbolsOfStringsIndexes[sortedListIndex] + 1) % currentLine.Length;
+                    string currentLineInIndexesList = line.Substring(currentLineInIndexesListStartIndex) +
+                        line.Substring(0, currentLineInIndexesListStartIndex);
+                    if (currentLine.CompareTo(currentLineInIndexesList) < 0)
+                    {
+                        break;
+                    }
+                    sortedListIndex++;
+                    if (sortedListIndex == sortedLastSymbolsOfStringsIndexes.Count)
+                    {
+                        break;
+                    }
+                }
+                sortedLastSymbolsOfStringsIndexes.Insert(sortedListIndex, currentLineLastSymbolIndex);
+            }
+
+            currentLine = currentLine.Substring(1) + currentLine.Substring(0, 1);
+            currentLineLastSymbolIndex = (currentLineLastSymbolIndex + 1) % currentLine.Length;
         }
-        Array.Sort(cyclicShifts);
 
         string result = "";
-        int endPosition = 0;
-        for (var i = 0; i < line.Length; i++)
+        int lastSymbolPosition = 0;
+        for (int i = 0; i < sortedLastSymbolsOfStringsIndexes.Count; i++)
         {
-            result += cyclicShifts[i][cyclicShifts[i].Length - 1];
-            if (line == cyclicShifts[i])
+            if (sortedLastSymbolsOfStringsIndexes[i] == sortedLastSymbolsOfStringsIndexes.Count - 1)
             {
-                endPosition = i;
+                lastSymbolPosition = i;
             }
+            result += line[sortedLastSymbolsOfStringsIndexes[i]];
         }
 
-        return (result, endPosition);
+        return (result, lastSymbolPosition);
     }
 
+    /// <summary>
+    /// Отсортировать строку в лексикографическом порядке
+    /// </summary>
+    private static string SortString(string input)
+    {
+        char[] characters = input.ToArray();
+        Array.Sort(characters);
+        return new string(characters);
+    }
+
+    /// <summary>
+    /// Обратное преобразование
+    /// </summary>
     public static string InverseBWT(string line, int endPosition)
     {
-        string[] table = new string[line.Length];
-        for (var i = 0; i < line.Length; i++)
+        // Строка для поиска первого вхождения символа, а затем замены этого символа на '-'
+        var lineCopy = line;
+        var sortedLine = SortString(line);
+
+        // Словарь, в котором ключ - позиция символа в сортированной строке, а значение - числовой массив
+        // из двух элементов, где первый - код символа в кодировке, второй - "индекс" пары (ключ, значение) в словаре.
+        // Индекс в смысле "первое вхождение символа из пары в строку"
+        var permutationDictionary = new Dictionary<int, int[]>();
+        
+        for (int i = 0; i < sortedLine.Length; i++)
         {
-            for (var j = 0; j < line.Length; j++)
-            {
-                table[j] = line[j] + table[j];
-            }
-            Array.Sort(table);
+            var currentSymbol = sortedLine[i];
+            var currentSymbolFirstOccurrenceIndex = lineCopy.IndexOf(currentSymbol);
+
+            lineCopy = lineCopy.Substring(0, currentSymbolFirstOccurrenceIndex) + '-' +
+                lineCopy.Substring(currentSymbolFirstOccurrenceIndex + 1);
+
+            permutationDictionary.Add(i, new int[2] { (int)currentSymbol, currentSymbolFirstOccurrenceIndex });
         }
-        return table[endPosition];
+
+        var result = "";
+        var currentIndex = endPosition;
+        for (int i = 0; i < line.Length; i++)
+        {
+            var valuesArray = permutationDictionary[currentIndex];
+            result += (char)valuesArray[0];
+            currentIndex = valuesArray[1];
+        }
+
+        return result;
     }
 
+    /// <summary>
+    /// Проверка корректности алгоритма
+    /// </summary>
     private static bool Checking(string line)
     {
         (string transformedLine, int endPosition) = DirectBWT(line);
