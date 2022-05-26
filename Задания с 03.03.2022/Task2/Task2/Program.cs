498db63f1b1e69b3869cc00500f31a0c64e6af33
RuslanBeresnev <rusber03102003@gmail.com>
2022-05-26 22:40:45 +0300

    Всё возвращено к прежнему виду (без BWT)

--- Задания с 03.03.2022/Task2/Task2/Program.cs
+++ Задания с 03.03.2022/Task2/Task2/Program.cs
@@ -2,45 +2,8 @@
 
 internal class Program
 {
-    /// <summary>
-    /// Вывести коэффициент сжатия без использования/с использованием BWT
-    /// </summary>
-    private static void CompareResultsWithAndWithoutBWT()
-    {
-        var filePaths = new string[2] { "..//..//..//TextFile.txt", "..//..//..//BinaryFile.bin" };
-        for (int i = 0; i < filePaths.Length; i++)
-        {
-            var filePath = filePaths[i];
-
-            // Сжатие без BWT
-            var fileInfo = new FileInfo(filePath);
-            LZW.Compress(filePath);
-            LZW.Decompress(filePath + ".zipped");
-            var compressedFileInfo = new FileInfo(filePath + ".zipped");
-            float compressionFactorWithoutBWT = compressedFileInfo.Length / (float)fileInfo.Length;
-
-            // Сжатие с BWT
-            fileInfo = new FileInfo(filePath);
-            var endPosition = BWT.DirectBWTInFile(filePath);
-            LZW.Compress(filePath);
-            LZW.Decompress(filePath + ".zipped");
-            BWT.InverseBWTInFile(filePath, endPosition);
-            compressedFileInfo = new FileInfo(filePath + ".zipped");
-            float compressionFactorWithBWT = compressedFileInfo.Length / (float)fileInfo.Length;
-
-            Console.WriteLine($"Сжатый файл типа {filePath.Split(".")[1]} составляет {compressionFactorWithoutBWT * 100}% от исходного" +
-                $"без использования BWT и {compressionFactorWithBWT * 100}% с использованием BWT");
-        }
-    }
-
     private static void Main(string[] args)
     {
-        if (args.Length == 1 && args[0] == "-t")
-        {
-            CompareResultsWithAndWithoutBWT();
-            return;
-        }
-
         if (args.Length != 2 || !File.Exists(args[0]) || (args[1] != "-c" && args[1] != "-u"))
         {
             Console.WriteLine("Ввод некорректен ...");
