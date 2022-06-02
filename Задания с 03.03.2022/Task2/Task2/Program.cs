e64144be7b6a451f5ca2758c231dfd94ef648f52
RuslanBeresnev <rusber03102003@gmail.com>
2022-06-02 20:08:13 +0300

    Correcting

--- Задания с 03.03.2022/Task2/Task2/Program.cs
+++ Задания с 03.03.2022/Task2/Task2/Program.cs
@@ -1,35 +1,27 @@
-﻿namespace LZW;
-
-internal class Program
+﻿if (args.Length != 2 || !File.Exists(args[0]) || (args[1] != "-c" && args[1] != "-u"))
 {
-    private static void Main(string[] args)
+    Console.WriteLine("Ввод некорректен ...");
+    return;
+}
+
+if (args[1] == "-c")
+{
+    LZW.LZW.Compress(args[0]);
+
+    var fileInfo = new FileInfo(args[0]);
+    var compressedFileInfo = new FileInfo(args[0] + ".zipped");
+    float compressionFactor = compressedFileInfo.Length / (float)fileInfo.Length;
+
+    Console.WriteLine("Сжатый файл с расширением .zipped создан");
+    Console.WriteLine($"Объём сжатого файла составляет {compressionFactor * 100}% от объёма исходного");
+}
+else if (args[1] == "u")
+{
+    var isDecompressCorrect = LZW.LZW.Decompress(args[0]);
+    if (!isDecompressCorrect)
     {
-        if (args.Length != 2 || !File.Exists(args[0]) || (args[1] != "-c" && args[1] != "-u"))
-        {
-            Console.WriteLine("Ввод некорректен ...");
-            return;
-        }
-
-        if (args[1] == "-c")
-        {
-            LZW.Compress(args[0]);
-
-            var fileInfo = new FileInfo(args[0]);
-            var compressedFileInfo = new FileInfo(args[0] + ".zipped");
-            float compressionFactor = compressedFileInfo.Length / (float)fileInfo.Length;
-
-            Console.WriteLine("Сжатый файл с расширением .zipped создан");
-            Console.WriteLine($"Объём сжатого файла составляет {compressionFactor * 100}% от объёма исходного");
-        }
-        else if (args[1] == "u")
-        {
-            var isDecompressCorrect = LZW.Decompress(args[0]);
-            if (!isDecompressCorrect)
-            {
-                Console.WriteLine("Сжатый файл оказался некорректным ...");
-                return;
-            }
-            Console.WriteLine("Разжатый файл создан");
-        }
+        Console.WriteLine("Сжатый файл оказался некорректным ...");
+        return;
     }
+    Console.WriteLine("Разжатый файл создан");
 }
